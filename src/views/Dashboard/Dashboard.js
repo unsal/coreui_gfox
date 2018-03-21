54f8834f193a7e75bbeeb735f80896891a7f4712
Ugur Unsal <unsal@unsal.org>
2018-03-21 21:46:24 +0300

    Commit

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -7,25 +7,13 @@ class Dashboard extends Component {
     return (
       <div>
         <Jumbotron>
-          {/* <h1 className="display-3">GFox GDPR v1.o</h1>
+          <h1 className="display-3">GFox GDPR v1.o</h1>
           <p className="lead">GFox bir Mevzuat Uyumluluk İşletimi Yazılımıdır</p>
           <hr className="my-2" />
           <p>Daha fazlası için lütfen yazılımın sağını solunu gönül rahatlığı ile kurcalayınız.</p>
           <p className="lead">
             <Button color="primary">Devam</Button>
-          </p> */}
-
-          <Media>
-            <Media left href="#">
-              <Media object data-src="gfox.png" alt="gfox" />
-            </Media>
-            <Media body>
-              <Media heading>
-                Media heading
-              </Media>
-              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-            </Media>
-          </Media>
+          </p>
 
         </Jumbotron>
     </div>
